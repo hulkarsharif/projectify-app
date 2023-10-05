c874d80040efb27d8b1bb4863a3d835fda56a82a
Hulkar Sharif <hulkar.sharif@gmail.com>
2023-10-05 17:31:30 -0400

    Finish the login part

--- projects-app/src/controller/user.controller.js
+++ projects-app/src/controller/user.controller.js
@@ -2,20 +2,43 @@ import { userService } from "../services/user.service.js";
 class UserController {
     signUp = async (req, res) => {
         const { body } = req;
-
         const input = {
             email: body.email,
-            preferredName: body.preferredName,
+            preferredFirstName: body.preferredFirstName,
             firstName: body.firstName,
             lastName: body.lastName,
             password: body.password
         };
-
         try {
             await userService.signUp(input);
-            res.status(201).json({ message: "Success" });
+            res.status(201).json({
+                message: "Success"
+            });
         } catch (error) {
             res.status(500).json({ message: error.message });
+        }
+    };
+    login = async (req, res) => {
+        const { body } = req;
+
+        const input = {
+            email: body.email,
+            password: body.password
+        };
+
+        try {
+            await userService.login(input);
+            res.status(200).json({
+                message: "Success"
+            });
+        } catch (error) {
+            let statusCode = 500;
+            if (error.message === "Invalid Credentials") {
+                statusCode = 401;
+            }
+            res.status(statusCode).json({
+                message: error.message
+            });
         }
     };
 }
--- projects-app/src/routes/user.route.js
+++ projects-app/src/routes/user.route.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import { userController } from "../controller/user.controller.js";
-
 const userRouter = Router();
 
 userRouter.post("/sign-up", userController.signUp);
+userRouter.post("/login", userController.login);
 
 export { userRouter };
