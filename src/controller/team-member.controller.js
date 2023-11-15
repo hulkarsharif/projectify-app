8fc2583c01fcb2a6a35b5b7efab669ba70aef6b5
Hulkar Sharif <hulkar.sharif@gmail.com>
2023-11-15 09:21:32 -0500

    Pass missinig INACTIVE status to service method

--- src/controller/team-member.controller.js
+++ src/controller/team-member.controller.js
@@ -71,7 +71,11 @@ class TeamMemberController {
     });
     deactivate = catchAsync(async (req, res) => {
         const { adminId, body } = req;
-        await teamMemberService.changeStatus(adminId, body.teamMemberId);
+        await teamMemberService.changeStatus(
+            adminId,
+            body.teamMemberId,
+            "INACTIVE"
+        );
         res.status(204).send();
     });
     reactivate = catchAsync(async (req, res) => {
