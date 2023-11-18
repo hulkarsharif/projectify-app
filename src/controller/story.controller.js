a7cde6f1d308a588d8c832223b656ea4d9a7b1d9
Hulkar Sharif <hulkar.sharif@gmail.com>
2023-11-17 20:31:13 -0500

    Finish the story controller create part

--- src/controller/story.controller.js
+++ src/controller/story.controller.js
@@ -1,10 +1,36 @@
 import { catchAsync } from "../utils/catch-async.js";
+import { CustomError } from "../utils/custom-error.js";
 
 class StoryController {
     create = catchAsync(async (req, res) => {
         const {
             body: { title, description, point, due, assigneeId, projectId }
         } = req;
+
+        const input = {
+            title: body.title,
+            description: body.description,
+            point: body.point,
+            due: body.due,
+            assigneeId: body.assigneeId,
+            projectId: body.projectId
+        };
+
+        if (
+            !input.title ||
+            !input.description ||
+            !input.point ||
+            !input.due ||
+            !input.assigneeId ||
+            !input.projectId
+        ) {
+            throw new CustomError(
+                "All fields are required: First name, Last name, Email, Position",
+                400
+            );
+        }
+        await storyService.create(body.input);
+        res.status(201).send();
     });
 }
 
