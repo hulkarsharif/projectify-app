55becfebcd106bb28cf01d43753510410dbafe7a
Hulkar Sharif <hulkar.sharif@gmail.com>
2023-11-19 21:34:59 -0500

    Create getAll method

--- src/controller/story.controller.js
+++ src/controller/story.controller.js
@@ -1,25 +1,64 @@
 import { catchAsync } from "../utils/catch-async.js";
 import { CustomError } from "../utils/custom-error.js";
-
+import { storyService } from "../services/story.service.js";
 class StoryController {
     create = catchAsync(async (req, res) => {
         const {
-            body: { title, description, point, due, assigneeId, projectId }
+            body: { title, description, point, due, assigneeId, projectId },
+            adminId
         } = req;
-
-        if (
-            !title ||
-            !description ||
-            !point ||
-            !due ||
-            !assigneeId ||
-            !projectId
-        ) {
-            throw new CustomError("All fields are required", 400);
+        if (!title || !projectId) {
+            throw new CustomError(
+                "All fields are required: Title, Description and Due date!",
+                400
+            );
         }
-        const story = await storyService.create(assigneeId, body);
-        res.status(201).json({ data: story });
+        const input = {
+            title,
+            description,
+            point,
+            due,
+            assigneeId,
+            projectId
+        };
+        const story = await storyService.create(input, adminId);
+        res.status(200).json({
+            data: story
+        });
+    });
+
+    getOne = catchAsync(async (req, res) => {
+        const { projectId, params } = req;
+        const story = await storyService.getOne(params.id, projectId);
+        res.status(200).json({
+            data: story
+        });
+    });
+
+    getAll = catchAsync(async (req, res) => {
+        const { projectId } = req;
+        const stories = await storyService.getAll(projectId);
+        res.status(200).json({
+            data: stories
+        });
+    });
+    update = catchAsync(async (req, res) => {
+        const { body, params, assigneeId } = req;
+        const update = {};
+        if (body.title) {
+            update.title = body.title;
+        }
+        if (body.description) {
+            update.description = body.description;
+        }
+        if (body.point) {
+            update.point = body.point;
+        }
+        if (!update.title && !update.description && !update.point) {
+            throw new CustomError("No update data provided", 400);
+        }
+        await storyService.update(params.id, assigneeId, update);
+        res.status(204).send();
     });
 }
-
 export const storyController = new StoryController();
